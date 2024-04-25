5791d848f30ae02e1cf26cb62e321b641d2da634
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-04-25 14:26:01 -0300

    Atualizando as importações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
-import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent,
-    MainComponent,
-    FooterComponent
+    HomeComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
