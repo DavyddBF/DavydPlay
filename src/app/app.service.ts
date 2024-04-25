2474b8babb3b075c83d18274fa9ce83a88b98bc5
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-04-25 14:16:01 -0300

    Adicionando script de serviços - api

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -5,5 +5,32 @@ import { Injectable } from '@angular/core';
 })
 export class AppService {
 
-  constructor() { }
+  api: string = 'http://localhost:3000/videos';
+
+  async listaVideos() {
+    const apiGET: Response = (await fetch(this.api));
+    return await apiGET.json();
+}
+
+  async buscaVideos(termoBuscado: string) {
+    const apiBusca = await fetch(`http://localhost:3000/videos?q=${termoBuscado}`);
+    return await apiBusca.json();
+  }
+
+  async criaVideo(titulo: string, descricao: string, url: string, imagem: string) {
+    const apiPOST: Response = await fetch(this.api, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        }),
+    });
+    
+    return await apiPOST.json();
+  }
 }
