db56dad922a2b0ff9f44a15f8b15238f50f4b168
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-04-25 18:11:36 -0300

    Sanitizando a URL de video

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'; /* Código recomendado pelo GPT */ 
+
+import { Cardvideos } from '../cardvideos';
 
 @Component({
   selector: 'app-video',
@@ -8,5 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './video.component.css'
 })
 export class VideoComponent {
+  @Input() dadosRecebidos!: Cardvideos;
 
+  /* Código do GPT */
+  videoURL!: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.videoURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.dadosRecebidos.url)
+  }
 }
