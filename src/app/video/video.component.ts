2b57cbff94fcf9e8d50b4c91945ce4baa1f2417e
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-04-25 19:05:13 -0300

    Atualizando

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -19,6 +19,6 @@ export class VideoComponent {
   constructor(private sanitizer: DomSanitizer) {}
 
   ngOnInit() {
-    this.videoURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.dadosRecebidos.url)
+    this.videoURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.dadosRecebidos.url);
   }
 }
